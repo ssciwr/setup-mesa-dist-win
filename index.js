ac8be0908cdc8c1dc55bccd469b9e4f4689d7848
Liam Keegan <liam@keegan.ch>
2022-08-26 12:32:15 +0200

    improve logging

--- index.js
+++ index.js
@@ -4,22 +4,29 @@ const exec = require('@actions/exec');
 
 async function run() {
   try {
+
     const repo = "https://github.com/pal1000/mesa-dist-win"
     const version = core.getInput('version');
     const build = core.getInput('build');
     const filename = `mesa3d-${version}-${build}.7z`
     const url = `${repo}/releases/download/${version}/${filename}`
-    console.log(`Downloading ${url} ...`);
+
+    core.startGroup('Downloading')
+    core.info(`Downloading ${url} ...`);
     const path_7z = await tc.downloadTool(url, 'mesa.7z');
-    await exec.exec('powershell.exe', ['ls']);
-    await exec.exec('powershell.exe', ['ls', path_7z]);
-    console.log(`Downloaded to ${path_7z}`);
-    console.log(`Extracting ${path_7z} ...`);
+    core.info(`Downloaded to ${path_7z}`);
+    core.endGroup()
+
+    core.startGroup('Extracting')
+    core.info(`Extracting ${path_7z} ...`);
     await exec.exec('7z.exe', ['x', path_7z]);
-    console.log(`Extracted`);
-    await exec.exec('powershell.exe', ['ls']);
-    console.log(`Running systemwidedeploy.cmd 1 ...`);
+    core.endGroup()
+
+    core.startGroup('Installing')
+    core.info(`Running systemwidedeploy.cmd 1 ...`);
     await exec.exec('powershell.exe', ['.\\systemwidedeploy.cmd', '1']);
+    core.endGroup()
+
   } catch (error) {
     core.setFailed(error.message);
   }
